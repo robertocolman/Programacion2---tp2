611c5e1984452565653259d3dac708e90a1a697c
robertocolman <robertocolman93@gmail.com>
2025-08-30 15:54:03 -0300

    primer comit

---
+++ cancion.py
@@ -0,0 +1,14 @@
+class cancion:
+    def __init__(self, nombre, duracion, genero):
+        self.nombre = nombre
+        self.duracion = duracion
+        self.genero = genero 
+    
+    def establecerNombre(self, nombre):
+        self.nombre = nombre
+
+    def establecerDuracion(self, duracion):
+        self.duracion = duracion
+
+    def establecerGenero(self, genero):
+        self.genero = genero
